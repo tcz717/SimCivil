3fa261f4a2776f62564aae77f129521b3789401d
unknown <>
2016-10-24 13:11:12 +0800

    add cache when render map

--- render/maprender.py
+++ render/maprender.py
@@ -1,24 +1,28 @@
 from model import tilemap
 import pygame
-import numpy as np
+from common import vector2
 
 class MapRender():
     def __init__(self,tmap,texture,block_size=(10,10),space=2):
         self.tilemap=tmap
-        self.block_size=np.array(block_size)
-        self.space=np.array((space,space))
+        self.block_size=vector2(block_size)
+        self.space=vector2(space,space)
         self.texture=texture
+        self.map_cache=None
 
     def render_map(self, screen,start_pos=(0,0)):
-        tiles= pygame.surface.Surface(np.array(self.block_size+self.space)*self.tilemap.size)
-        for i in range(self.tilemap.size[0]):
-            for j in range(self.tilemap.size[1]):
-                tile=self.tilemap[i,j]
-                pos =np.array(self.block_size+self.space)*(i,j)
-                tiles.blit(self.texture.load(tile.tid,tuple(self.block_size)),pos)
+        if not self.map_cache:
+            tiles= pygame.surface.Surface(vector2(self.block_size+self.space)*self.tilemap.size)
+            for i in range(self.tilemap.size[0]):
+                for j in range(self.tilemap.size[1]):
+                    tile=self.tilemap[i,j]
+                    pos =vector2(self.block_size+self.space)*(i,j)
+                    tiles.blit(self.texture.load(tile.tid,tuple(self.block_size)),pos)
+            self.map_cache=tiles
                 
-        screen.blit(tiles,start_pos)
+        screen.blit(self.map_cache,start_pos)
 
     def render_entity(self,screen,entity,start_pos=(0,0)):
-        pos =np.array(self.block_size+self.space)*entity.position
+        center=(entity.precise_position[0]-0.5,entity.precise_position[1]-0.5)
+        pos =vector2(self.block_size+self.space)*center
         entity.render(screen,pos,self.block_size)
