c687422f9a5b3925ca7cbb2648621bbe4e49824a
Tangent <tcz717@hotmail.com>
2016-10-26 18:03:10 +0800

    support screen scroll add entity generator

--- main.py
+++ main.py
@@ -5,34 +5,50 @@ from render.maprender import MapRender
 from model.tilemap import TileMap
 from model.entity import Human,HumanStatus
 from model.behavior import BehaviorInterface
-from generator import MapGenerator
+from generator import MapGenerator,EntityGenerator
+from common import vector2
 from pygame.locals import *
 from sys import exit
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480), 0, 32)
-pygame.display.set_caption("Sim Stick")
+size=(640, 480)
+map_position=vector2(0,0)
+screen = pygame.display.set_mode(size, 0, 32)
+pygame.display.set_caption("Sim Civil")
 fontobj = pygame.font.SysFont('arial', 15)
 
-mgen=MapGenerator((30,30))
-tmap=mgen.generate()
+mgen=MapGenerator()
+tmap=mgen.generate((100,100))
 cache=texture_manage.DefaultTexture()
-render=MapRender(tmap,cache,block_size=(15,15))
+render=MapRender(tmap,cache,size,block_size=(15,15))
 
-man = Human(tmap,(9,8),"test")
-behavior=BehaviorInterface(man)
-man.behavior=behavior
-man[HumanStatus.walk_speed]=2.0
+egen=EntityGenerator(tmap)
+man=egen.get_std_human("test")
+behavior=man.behavior
 
 clock=pygame.time.Clock()
 while True:
-    tx,ty=pygame.mouse.get_pos()
-    tx,ty=tx/(render.block_size[0]+render.space[0]),ty/(render.block_size[1]+render.space[1])
+    tx,ty=render.screen2map(pygame.mouse.get_pos())
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
         if event.type == MOUSEBUTTONDOWN:
-            behavior.move((tx,ty))
+            if pygame.mouse.get_pressed()[0]:
+                behavior.move((tx,ty))
+
+    pressed_keys = pygame.key.get_pressed()
+    rel=pygame.mouse.get_rel()
+    if pygame.mouse.get_pressed()[2]:
+        map_position=map_position-rel
+    if pressed_keys[K_LEFT]:
+        map_position.x = map_position.x-5
+    elif pressed_keys[K_RIGHT]:
+        map_position.x = map_position.x+5
+    if pressed_keys[K_UP]:
+        map_position.y = map_position.y-5
+    elif pressed_keys[K_DOWN]:
+        map_position.y = map_position.y+5
+
     screen.fill((0, 0, 0))
 
     time_passed=clock.tick(50)/1000.0
@@ -41,10 +57,10 @@ while True:
     if time_passed>0.1:
         print time_passed
 
-    render.render_map(screen)
-    render.render_entity(screen,man)
+    render.render_map(screen,map_position)
+    render.render_entity(screen,man,map_position)
     screen.blit(fontobj.render(
-        str(int(1/time_passed))+' '+str(man.position),
+        str(clock.get_fps())+' '+str(man.position)+' '+str(map_position),
         True,
         (255,0,0)
         ),(0,0))
