f96e07c7f01b5ec61f7f377ee5ce838be414a4fc
Yongzhen Pan <250224740@qq.com>
2017-08-16 18:34:30 +0800

    Built server basic function
    Can handle TcpListener and TcpClient in multithread to read and send packages.

--- SimCivil/Program.cs
+++ SimCivil/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using SimCivil.Net;
 
 namespace SimCivil
 {
@@ -6,8 +8,50 @@ namespace SimCivil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Read();
+            int port;
+            do
+                Console.WriteLine("Enter port to start server:");
+            while (!int.TryParse(Console.ReadLine(), out port));
+
+            // Define send and read package queue
+            Queue<Package> packageSendQueue = new Queue<Package>();
+            Queue<Package> packageReadQueue = new Queue<Package>();
+
+            // Enqueue some send tests
+            for (int i = 0; i < 20; i++)
+            {
+                packageSendQueue.Enqueue(new Package("###test string###---" + i));
+            }
+
+            // Initialize server listener and start
+            SeverListener server = new SeverListener(port, packageSendQueue, packageReadQueue);
+            server.Start();
+
+            #region send test
+            while (true)
+            {
+                Package pk = new Package(Console.ReadLine());
+                lock (packageSendQueue)
+                {
+                    packageSendQueue.Enqueue(pk);
+                }
+            }
+            #endregion
+
+            #region read test
+            //while (true)
+            //{
+            //    lock (packageReadQueue)
+            //    {
+            //        if (packageReadQueue.Count > 0)
+            //        {
+            //            Console.WriteLine(packageReadQueue.Dequeue().S);
+            //        }
+            //    }
+            //}
+            #endregion
+
+            Console.ReadLine();
         }
     }
 }
