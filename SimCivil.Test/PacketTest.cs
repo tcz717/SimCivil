f37723fcb203e2b5f7211df2ce0a773e71f1a58e
tcz717 <tcz717@hotmail.com>
2017-08-16 13:18:12 -0400

    test:加入测试示例
    Signed-off-by: tcz717 <tcz717@hotmail.com>

---
+++ SimCivil.Test/PacketTest.cs
@@ -0,0 +1,65 @@
+﻿using SimCivil.Net;
+using SimCivil.Net.Packets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace SimCivil.Test
+{
+    public class PacketTest
+    {
+        private byte[] ToData(int id, PacketType type, int size)
+        {
+            MemoryStream stream = new MemoryStream();
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write(id);
+                writer.Write((int)type);
+                writer.Write(size);
+            }
+            return stream.ToArray();
+        }
+        [Theory]
+        [InlineData(1, PacketType.Ping, 3)]
+        public void HeadToBytes(int id, PacketType type, int size)
+        {
+            var head = new Head(id, type, size);
+
+            var data = ToData(id, type, size);
+
+            Assert.Equal(data, head.ToBytes());
+        }
+        [Theory]
+        [InlineData(1, PacketType.Ping, 3)]
+        public void HeadFrombytes(int id, PacketType type, int size)
+        {
+            var data = ToData(id, type, size);
+            var head = Head.FromBytes(data);
+
+            Assert.Equal(head.packageID, id);
+            Assert.Equal(head.type, type);
+            Assert.Equal(head.length, size);
+        }
+
+        [Fact]
+        public void ToBytes()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData(1, PacketType.Ping, 0)]
+        public void PacketCreate(int id, PacketType type, int size)
+        {
+            var pkt = PacketFactory.Create(
+                new ServerClient(null, null),
+                new Head(id, type, size),
+                new byte[0]
+                );
+            Assert.IsType<Ping>(pkt);
+        }
+    }
+}
---
+++ SimCivil/Net/Packets/Ping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimCivil.Net.Packets
+{
+    public class Ping : Packet
+    {
+        public Ping(Dictionary<string, object> data, Head head = default(Head), ServerClient client = null) : base(data, head, client)
+        {
+        }
+
+        public override void Handle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
