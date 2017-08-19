2c2cc89974d6216f0148625b950110fb9165ede3
tcz717 <tcz717@hotmail.com>
2017-08-19 15:12:24 -0400

    test: add test about fake inject.
    Signed-off-by: tcz717 <tcz717@hotmail.com>

--- SimCivil.Test/SimCivilTest.cs
+++ SimCivil.Test/SimCivilTest.cs
@@ -1,5 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.FakeItEasy;
+using FakeItEasy;
+using SimCivil.Map;
 using SimCivil.Net;
+using SimCivil.Store;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,6 +25,27 @@ namespace SimCivil.Test
             Assert.NotNull(second);
 
             Assert.Equal(first, second);
+        }
+
+        [Fact]
+        public void InjectTest()
+        {
+            using (var service = new AutoFake())
+            {
+                var map = service.Resolve<MapData>();
+                Assert.IsType<MapData>(map);
+            }
+
+            var builder = new ContainerBuilder();
+            builder.Register(n => A.Fake<IMapGenerator>());
+            builder.Register(n => A.Fake<IMapRepository>());
+            builder.RegisterType<MapData>().SingleInstance();
+
+            using (var Services = builder.Build())
+            {
+                var map = Services.Resolve<MapData>();
+                Assert.IsType<MapData>(map);
+            }
         }
     }
 }
