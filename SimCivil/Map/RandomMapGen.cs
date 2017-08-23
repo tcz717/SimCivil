bd4f615dcba668309d4dc34652a7a3bc1c0b9d09
tcz717 <tcz717@hotmail.com>
2017-08-23 09:05:14 -0400

    add: RandomMapGen
    Signed-off-by: tcz717 <tcz717@hotmail.com>

---
+++ SimCivil/Map/RandomMapGen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static SimCivil.Config;
+
+namespace SimCivil.Map
+{
+    /// <summary>
+    /// Random map generator used for test.
+    /// </summary>
+    public class RandomMapGen : IMapGenerator
+    {
+        private Random rand;
+
+        public event GeneratingEventHandler OnGenerating;
+
+        public Atlas Generate(int x, int y, int width = DefaultAtlasWidth, int height = DefaultAtlasHeight)
+        {
+            Atlas atlas = Atlas.Create(x, y, width, height);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    atlas.Tiles[i, j] = Tile.Create((x * width + i, y * height + j),
+                        height: rand.Next(SeaLevel - 10, SeaLevel + 10));
+                    OnGenerating?.Invoke(this, new GeneratingEventArgs((i, j), atlas.Tiles[i, j]));
+                }
+            }
+            return atlas;
+        }
+
+        public RandomMapGen()
+        {
+            rand = new Random(Cfg.Seed);
+        }
+    }
+}
