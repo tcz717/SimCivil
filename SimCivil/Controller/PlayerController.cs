5b33b54c2def6be3d40fc2fc415a779ffc1a9d29
tcz717 <tcz717@hotmail.com>
2017-08-23 09:05:14 -0400

    add: framework for controller
    Signed-off-by: tcz717 <tcz717@hotmail.com>

---
+++ SimCivil/Controller/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using SimCivil.Map;
+using SimCivil.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimCivil.Controller
+{
+    public class PlayerController : IController
+    {
+        private MapData Map;
+        private IServerListener Server;
+
+        public void Update(int tickCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            Server.OnNewConnected += Server_OnNewConnected;
+        }
+
+        private void Server_OnNewConnected(object sender, ServerClient e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            Server.OnNewConnected -= Server_OnNewConnected;
+        }
+
+        public PlayerController(MapData map, IServerListener server)
+        {
+            Map = map;
+            Server = server;
+        }
+    }
+}
