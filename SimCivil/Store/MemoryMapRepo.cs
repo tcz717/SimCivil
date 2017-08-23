335d9db6d906c8b167fdda671ca5bab36e3d51de
tcz717 <tcz717@hotmail.com>
2017-08-23 09:05:14 -0400

    add:MemoryMapRepo
    Signed-off-by: tcz717 <tcz717@hotmail.com>

---
+++ SimCivil/Store/MemoryMapRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimCivil.Map;
+
+namespace SimCivil.Store
+{
+    /// <summary>
+    /// A map repository storing atlas in memory. Just designed for test.
+    /// </summary>
+    public class MemoryMapRepo : IMapRepository
+    {
+        private Dictionary<(int X, int Y), Atlas> AtlasStore;
+
+        public bool Contains((int X, int Y) atlasIndex) => AtlasStore.ContainsKey(atlasIndex);
+
+        public Atlas GetAtlas((int X, int Y) atlasIndex) => AtlasStore[atlasIndex];
+
+        public void PutAtlas((int X, int Y) atlasIndex, Atlas atlas) => AtlasStore[atlasIndex] = atlas;
+
+        public MemoryMapRepo()
+        {
+            AtlasStore = new Dictionary<(int X, int Y), Atlas>();
+        }
+    }
+}
