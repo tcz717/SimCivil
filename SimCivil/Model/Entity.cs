d5d2fe74915786dff651fae00ca588a1d8ce425b
Tangent <tcz717@hotmail.com>
2018-01-06 01:35:45 -0500

    [change] Entity.Meta to Dictionary

--- SimCivil/Model/Entity.cs
+++ SimCivil/Model/Entity.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Xml.Schema;
+
 using JetBrains.Annotations;
 using SimCivil.Store.Json;
 using static SimCivil.Config;
@@ -72,8 +74,7 @@ namespace SimCivil.Model
         /// <value>
         /// The meta.
         /// </value>
-        [JsonConverter(typeof(NullableDynamicObjectConverter))]
-        public dynamic Meta { get; set; }
+        public Dictionary<string,object> Meta { get; set; }
 
         /// <summary>
         /// Occurs when [position changed].
@@ -88,7 +89,7 @@ namespace SimCivil.Model
         {
             return new Entity(Guid.NewGuid())
             {
-                Meta = new NullableDynamicObject(),
+                Meta = new Dictionary<string, object>(),
             };
         }
 
@@ -102,7 +103,7 @@ namespace SimCivil.Model
             {
                 Name = Name,
                 Position = Cfg.SpawnPoint,
-                Meta = Meta.Clone(),
+                Meta = new Dictionary<string, object>(Meta),
                 Type = Type
             };
         }
@@ -131,7 +132,7 @@ namespace SimCivil.Model
         /// </returns>
         public bool Equals(Entity other)
         {
-            if (ReferenceEquals(null, other)) return false;
+            if (other is null) return false;
             if (ReferenceEquals(this, other)) return true;
             return Id.Equals(other.Id);
         }
@@ -145,7 +146,7 @@ namespace SimCivil.Model
         /// </returns>
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
+            if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != GetType()) return false;
             return Equals((Entity) obj);
