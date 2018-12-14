5e93fdb1ea2938f4e1bc8be94d3a947ad13ea296
Tangent <tcz717@hotmail.com>
2018-12-14 03:17:48 -0500

    Unlimit maxthreads

--- xunit.bash
+++ xunit.bash
@@ -1,6 +1,12 @@
-if [[ -z "${DOTNETCORE}" ]]; then
-	dotnet test SimCivil.Test --filter "Category!=Performance"
+if [[ ${DOTNETCORE} ]]; then
+	# dotnet test SimCivil.Test --filter "Category!=Performance" -f netcoreapp2.0
+	dotnet build SimCivil.Test -f netcoreapp2.0
+	dotnet ./xunit.runner.console.*/tools/netcoreapp2.0/xunit.console.dll   \
+	./SimCivil.Test/bin/Debug/netcoreapp2.0/SimCivil.Test.dll               \
+	-maxthreads unlimited -notrait "Category=Performance"
 else
 	msbuild SimCivil.Test
-	mono ./testrunner/xunit.runner.console.*/tools/net461/xunit.console.exe ./SimCivil.Test/bin/Debug/net461/SimCivil.Test.dll
+	mono ./testrunner/xunit.runner.console.*/tools/net461/xunit.console.exe \
+	./SimCivil.Test/bin/Debug/net461/SimCivil.Test.dll                      \
+	-maxthreads unlimited -notrait "Category=Performance"
 fi
