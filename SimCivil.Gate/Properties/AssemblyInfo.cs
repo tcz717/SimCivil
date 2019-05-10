32a4a620affa73f3ffb3d0b33871710d7047e66c
Chengzhi Tan <chengzht@amazon.com>
2019-05-10 12:25:00 -0700

    Allow version patch working

---
+++ SimCivil.Gate/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+// 方法是按如下所示使用“*”: :
+// [assembly: AssemblyVersion("1.0.*")]
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.0.0")]
