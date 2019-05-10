1ccd169ebeb5453004335cd1018ae14d379a9734
Chengzhi Tan <tcz717@hotmail.com>
2019-05-10 00:35:24 -0700

    Add deploy script debug information

--- SimCivil.Orleans.Server/before-deploy.ps1
+++ SimCivil.Orleans.Server/before-deploy.ps1
@@ -1,4 +1,4 @@
 ﻿$name = "SimCivil.Orleans.Server"
-if ((Get-Process).Where({ $_.Name -Match $name }, 'First').Count -gt 0 ) {
-	Stop-Process -Name $name
+if ((Get-Process).Where({ $_.Name -Match $name }, 'First').Count -gt 0 -and $env:APPLICATION_PATH -Match "net461") {
+	Write-Output (Stop-Process -Name $name)
 }
