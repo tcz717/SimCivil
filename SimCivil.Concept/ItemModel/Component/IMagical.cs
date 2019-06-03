f872add3c474888ab63116aed9768074b2fdf1df
Yongzhen Pan <250224740@qq.com>
2019-06-03 10:44:24 +0800

    Add magical part

---
+++ SimCivil.Concept/ItemModel/Component/IMagical.cs
@@ -0,0 +1,51 @@
+﻿using SimCivil.Orleans.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimCivil.Concept.ItemModel.Component
+{
+    interface IMagical : IComponent
+    {
+        /// <summary>
+        /// Clears elements.
+        /// </summary>
+        /// <returns></returns>
+        Task<Result> Clear();
+
+        /// <summary>
+        /// Gets the magic elements.
+        /// </summary>
+        /// <returns></returns>
+        Task<Result<IDictionary<MagicElement, double>>> GetMagicElements();
+
+        /// <summary>
+        /// Clears and sets the magic elements.
+        /// </summary>
+        /// <param name="elements">The elements.</param>
+        /// <returns></returns>
+        Task<Result> SetMagicElements(IDictionary<MagicElement, double> elements);
+
+        /// <summary>
+        /// Adds the magic elements.
+        /// </summary>
+        /// <param name="elements">The elements.</param>
+        /// <returns></returns>
+        Task<Result> AddMagicElements(IDictionary<MagicElement, double> elements);
+
+        /// <summary>
+        /// Updates the magic elements.
+        /// </summary>
+        /// <param name="elements">The elements.</param>
+        /// <returns></returns>
+        Task<Result> UpdateMagicElements(IDictionary<MagicElement, double> elements);
+
+        /// <summary>
+        /// Removes the magic elements.
+        /// </summary>
+        /// <param name="elements">The elements.</param>
+        /// <returns></returns>
+        Task<Result> RemoveMagicElements(IEnumerable<MagicElement> elements);
+    }
+}
---
+++ SimCivil.Concept/ItemModel/Component/State/MagicalState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimCivil.Concept.ItemModel.Component.State
+{
+    class MagicalState
+    {
+        public IDictionary<MagicElement, double> ElementQuantities { get; set; }
+    }
+}
